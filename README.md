30645cdb5fcffd22001cf9ecc365fdaf85552298
Ax_6 <axolo6@gmail.com>
2016-12-17 19:40:46 +1100

    Updated readme and code

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # log-log
-Easy and friendly logging.
+Easy and friendly logging
+
+## Installation
+```bash
+$ npm install log-log
+```
+
+## Examples
+Ready to go
+```js
+var LogLog = require('log-log');
+var log = new LogLog();
+
+log.debug("Debug some stuff");
+```
+Simple demo
+```js
+var log = new LogLog({applicationName: "Demo", color: LogLog.COLORS.CYAN});
+
+log.debug("Hello!");
+
+log.changeColor(LogLog.COLORS.DEFAULT);
+
+log.error("Sample error");
+log.warning("Sample warning");
+log.success("Sample success");
+log.test("Sample test");
+```
+![example](https://i.gyazo.com/a3cca4a6edba039d375aee36b58dcdc5.png)
+
+## Methods
+### debug
+**Parameters**
+ - message {String | Object}
+ - [depthLevel] {Number} Optional
+When passing objects they get stringified
+
+error | warning | success | test
+**Parameters**
+ - message {String | Object}
+When passing objects they get stringified
+
+### disableDebug | enableDebug
+Will affect only .debug messages
+
+### isDebugEnabled
+Boolean returned
+
+### setDepthLevel
+**Parameters**
+ - level {Number}
+You can specify debug depth in order to filter unwanted deep debug messages
+
+### changeColor
+**Parameters**
+ - color {String}
+You can access available colors from LogLog.COLORS object
--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * Created by aaronrusso on 16/12/2016.
  */
 module.exports = Logs;
-var events = require("events");
-var chalk = require("chalk");
+var util = require('util');
+var events = require('events');
+var chalk = require('chalk');
 
 /**
  * General purpose Logs class
@@ -19,16 +20,16 @@ var chalk = require("chalk");
  * @returns {Logs}
  */
 function Logs(logOptions) {
-    this.application = "";
-    this.color = null;
+    this.applicationName = "";
+    this.color = Logs.COLORS.DEFAULT;
     this.dim = false;
-    if (logOptions.hasOwnProperty("applicationName")) {
-        this.application = logOptions.applicationName;
+    if (logOptions && logOptions.hasOwnProperty("applicationName")) {
+        this.applicationName = logOptions.applicationName;
     }
-    if (logOptions.hasOwnProperty("color")) {
-        this.color = logOptions.color;
+    if (logOptions && logOptions.hasOwnProperty("color") && this._colorExist(logOptions.color)) {
+        this.changeColor(logOptions.color.toLowerCase());
     }
-    if (logOptions.hasOwnProperty("dim")) {
+    if (logOptions && logOptions.hasOwnProperty("dim")) {
         this.dim = logOptions.dim;
     }
     this._debugFlag = true;
@@ -36,41 +37,78 @@ function Logs(logOptions) {
     events.EventEmitter.call(this);
 }
 
-require("util").inherits(Logs, events.EventEmitter);
+util.inherits(Logs, events.EventEmitter);
 
-Logs.HIGH_LEVEL = 0;
-Logs.MEDIUM_LEVEL = 1;
-Logs.LOW_LEVEL = 2;
+Logs.HIGH_DEPTH = 0;
+Logs.MEDIUM_DEPTH = 1;
+Logs.LOW_DEPTH = 2;
+
+Logs.COLORS = {
+    DEFAULT: null,
+    RED: "red",
+    GREEN: "green",
+    YELLOW: "yellow",
+    BLUE: "blue",
+    MAGENTA: "magenta",
+    CYAN: "cyan",
+    WHITE: "white",
+    GRAY: "gray",
+    BLACK: "black"
+};
 
 /**
  * Log error
- * @param {String} error
+ * @param {String|Object} error
  */
 Logs.prototype.error = function (error) {
-    this._msg(error, chalk.red.bold.underline("ERROR # "));
+    this._msg(error, chalk.bgRed.black(" ERROR ") + " ");
 };
 
 /**
  * Log warning
- * @param {String} warning
+ * @param {String|Object} warning
  */
 Logs.prototype.warning = function (warning) {
-    this._msg(warning, chalk.yellow.bold.underline("WARNING # "));
+    this._msg(warning, chalk.bgYellow.black(" WARNING ") + " ");
+};
+
+/**
+ * Log success
+ * @param {String|Object} success
+ */
+Logs.prototype.success = function (success) {
+    this._msg(success, chalk.bgGreen.black(" SUCCESS ") + " ");
 };
 
 /**
  * Log message
- * @param {String} msg
+ * @param {String|Object} msg
  * @param {Number} [level] Define debug depth to consider
  */
 Logs.prototype.debug = function (msg, level) {
     if (this.isDebugEnabled() && (!level || level <= this.level)) {
-        this._msg(" " + msg);
+        this._msg(msg);
     }
 };
 
+/**
+ * Log test
+ * @param {String|Object} msg
+ */
 Logs.prototype.test = function (msg) {
-    this._msg(msg, chalk.cyan.bold("~TEST~"));
+    this._msg(msg, chalk.cyan.bold("~TEST~ "));
+};
+
+/**
+ * Change color
+ * @param {String} color
+ */
+Logs.prototype.changeColor = function (color) {
+    if (this._colorExist(color)) {
+        this.color = color;
+    } else {
+        this.color = null;
+    }
 };
 
 /**
@@ -81,14 +119,6 @@ Logs.prototype.setDepthLevel = function (level) {
     if (!isNaN(level)) {
         this.level = level;
     }
-};
-
-Logs.prototype._msg = function (_msg, _preMsg) {
-    if (!_preMsg) {
-        _preMsg = "";
-    }
-    var msg = chalk.dim(this.getNiceDate()) + " <" + this.application + "> " + (this.color ? chalk[this.color](_msg) : _msg);
-    console.log(_preMsg + (!this.dim ? msg : chalk.dim(msg)));
 };
 
 /**
@@ -113,7 +143,39 @@ Logs.prototype.disableDebug = function () {
     this._debugFlag = false;
 };
 
-Logs.prototype.getNiceDate = function (mode, myDate) {
+Logs.prototype._msg = function (_msg, _preMsg) {
+    if (!_preMsg) {
+        _preMsg = "";
+    }
+    var msg = chalk.dim(this._getNiceDate()) + this._getFormattedName() + this._getFormattedMessage(_msg);
+    console.log(_preMsg + (!this.dim ? msg : chalk.dim(msg)));
+};
+
+Logs.prototype._getFormattedMessage = function (_msg) {
+    if (typeof _msg === "object") {
+        _msg = JSON.stringify(util.inspect(_msg));
+    }
+    return (this.color ? chalk[this.color](_msg) : _msg);
+};
+
+Logs.prototype._getFormattedName = function () {
+    if (this.applicationName) {
+        return " <" + this.applicationName + "> ";
+    } else {
+        return "";
+    }
+};
+
+Logs.prototype._colorExist = function (color) {
+    for (var existingColor in Logs.COLORS) {
+        if (Logs.COLORS[existingColor] === color) {
+            return true;
+        }
+    }
+    return false;
+};
+
+Logs.prototype._getNiceDate = function (mode, myDate) {
     var date = new Date();
     if (myDate) {
         date = myDate;
